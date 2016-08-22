33753917f36901a8c8033be109fb76435b44e3b2
silent-mobius <michael@linuxsystems.rocks>
2016-08-23 01:08:37 +0300

    initial script to define the linux/unix type OS used

--- OS_fingerprint.py
+++ OS_fingerprint.py
@@ -1,40 +1,54 @@
 #!/usr/bin/env python
-
 import platform
-
-class OpSysType(Obj):
-	
-	
-	
-	def __getattr__(self,attr):
-		if attr = 'osx':
-			return 'osx'
-		elif attr = 'rhel':
-			return 'RedHat'
-		elif attr = 'ubu':
-			return 'Ubuntu'
-		elif attr = 'fbsd':
-			return 'FreeBSD'
-		elif attr = 'sun':
-			return 'SunOS'
-		elif attr = 'unknown_linux':
-			return 'unknown_linux'
-		elif attr = 'unknown':
-			return 'unknown'
+"""
+Fingerprints the following Operating Systems:
+* Mac OS X
+* Ubuntu
+* Red Hat/Cent OS
+* FreeBSD
+* SunOS
+"""
+class OpSysType(object):
+	def __getattr__(self, attr):
+		if attr == "osx":
+			return "osx"
+		elif attr == "rhel":
+		  return "redhat"
+		elif attr == "ubu":
+		  return "ubuntu"
+		elif attr == "fbsd":
+		  return "FreeBSD"
+		elif attr == "sun":
+		  return "SunOS"
+		elif attr == "unknown_linux":
+		  return "unknown_linux"
+		elif attr == "unknown":
+		  return "unknown"
 		else:
-			raise AttributeError, attr
-			
-			
+		  raise AttributeError, attr
+		  
+		  
 	def linuxType(self):
 		if platform.dist()[0] == self.rhel:
 			return self.rhel
 		elif platform.uname()[1] == self.ubu:
-			return self.ubu
-		#TODO add as much distro as possible.
+		  return self.ubu
 		else:
-			return self.unknown_linux
-			
-			
+		  return self.unknown_linux
+	
 	def queryOS(self):
-		if platform.system() == 'Darwin':
+		if platform.system() == "Darwin":
 			return self.osx
+		elif platform.system() == "Linux":
+		  return self.linuxType()
+		elif platform.system() == self.sun:
+		  return self.sun
+		elif platform.system() == self.fbsd:
+			  return self.fbsd
+			  
+def fingerprint():
+	type = OpSysType()
+	print type.queryOS()
+		
+if __name__ == "__main__":
+		fingerprint()
