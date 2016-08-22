a65bc21370039f13816206ea78511f63cd2ccdc2
silent-mobius <michael@linuxsystems.rocks>
2016-08-22 13:24:55 +0300

    slowely learning the lib apt and how to use it

--- OS_Setup.py
+++ OS_Setup.py
@@ -31,17 +31,86 @@
 #lib import
 import os
 import sys
+import apt
 import platform
 import subprocess
 import argparse
 
 
 ##Vars::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-REPO =
+var = platform.dist()
+REPO = var[0]
 USER = "mobius" ### place your user name here
 PASSWD = "1"         ### palce your passwd here
-REPONAME =
-KODENAME =
-ARCH =
-VERSION =
+REPONAME = var[0]
+KODENAME = 
+ARCH = platform.machine()
+VERSION = var[1]
 INSTALL_MNGR =
+DISTROS = {
+		"Ubuntu" : "/etc/lsb-release",
+		"Debian" : "/etc/debian_version",
+		"RedHat" : "/etc/redhat-release",
+		"SUSE" : "/etc/SUSE-release",
+		"Fedora" : "/etc/fedora-release",
+		"Gentoo" : "/etc/gentoo-release",
+		"Slackware" : "/etc/slackware-version",
+		"Mandriva" : "/etc/mandriva-release",
+		"Mandrake" : "/etc/mandrake-release",
+		"YellowDog" : "/etc/yellowdog-release",
+		"SUN JDS" : "/etc/sun-release",
+	}
+packages = ['lightdm','mate-desktop-environment-extras', 'firmware-realtek', 'firmware-linux', 'firmware-linux-free',
+'firmware-linux-nonfree', 'vlc', 'gparted', 'abiword', 'transmission', 'guake', 'mixxx', 'culmus', 'xfonts-efont-unicode',
+'xfonts-efont-unicode-ib', 'xfonts-intl-european', 'msttcorefonts', 'sqlite', 'sqlite3', 'mysql-client', 'mysql-server',
+'postgresql', 'apache2', 'nginx-full', 'nfs-common', 'samba-common', 'redis-server', 'sysv-rc-conf', 'wget', 'curl', 'nmap',
+'zenmap', 'aircrack-ng', 'dsniff', 'ndiff', 'nbtscan', 'wireshark', 'tshark', 'tcpdump', 'netcat', 'macchanger', 'python-scapy',
+'python-pip', 'python-networkx', 'python-netaddr', 'python-netifaces', 'python-netfilter', 'python-gnuplot', 'python-mako',
+'python-radix', 'ipython', 'python-pycurl', 'python-lxml', 'python-libpcap', 'python-nmap', 'python-flask', 'python-scrapy',
+'libpoe-component-pcap-perl', 'libnet-pcap-perl', 'perl-modules', 'geany', 'build-essential', 'debhelper', 'cmake', 'bison',
+'flex', 'libgtk2.0-dev', 'libltdl3-dev', 'libncurses-dev', 'libusb-1.0-0-dev', 'git', 'git-core', 'libncurses5-dev',
+'libnet1-dev', 'libpcre3-dev', 'libssl-dev', 'libcurl4-openssl-dev', 'ghostscript', 'autoconf', 'python-software-properties',
+'debian-goodies', 'freeglut3-dev', 'libxmu-dev', 'libpcap-dev', 'libglib2.0', 'libxml2-dev', 'libpcap-dev', 'libtool',
+'rrdtool', 'autoconf', 'automake', 'autogen', 'redis-server', 'libsqlite3-dev', 'libhiredis-dev', 'libgeoip-dev',
+'debootstrap', 'qemu-user-static', 'device-tree-compiler', 'lzma', 'lzop', 'pixz', 'dkms', 'gnupg', 'flex', 'bison', 'gperf',
+'libesd0-dev', 'zip', 'curl', 'libncurses5-dev', 'zlib1g-dev', 'gcc-multilib', 'g++-multilib', 'libusb-1.0-0',
+'libusb-1.0-0-dev', 'fakeroot', 'kernel-package', 'zlib1g-dev', 'devscripts', 'pbuilder', 'dh-make', 'mingw32',
+'mingw32-binutils', 'guake']
+pack_to_install = []
+###Func/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+
+def bash(var):
+	os.system(var)
+
+
+'''def distrofind(discreet):
+
+	for name,location in DISTROS.iteritems():
+		try:
+			distroFile = open("%s" % location,"r")
+			content = distroFile.readlines(); distroFile.close();
+			if discreet == 0:
+				IDfile = open("%s/DistroID" % cwd,"a")
+				IDfile.writelines(content); IDfile.close();
+				print "The contents of the identification file has been created at"
+				print "%s/DistroID for easy reference. You may use Python to " % cwd
+				print "parse it and obtain advanced information about the user's OS."
+			else: pass
+			return name
+		except IOError:
+			issueFile = open("/etc/issue","r")
+			issue = issueFile.readline(); issueFile.close()
+			qname = issue.split(); issue = qname[0];
+			return issue
+'''
+def edit_files():
+	if 
+	 
+def pac_check(pkg):
+	cache = apt.Cache()
+	
+	pack = cache[pkg]
+	if pack.is_installed:
+		pass
+	else:
+		
