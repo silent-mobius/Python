f00a2188aa3a1ded060fcef5fdaca62dcba2d3ca
silent-mobius <michael@linuxsystems.rocks>
2016-10-27 04:37:35 +0300

    script to create draft .rst file  for pelican

---
+++ gen_entry.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+
+###import libs
+import sys
+from datetime import datetime
+
+##creating template to generate
+TEMPLATE = """
+{title}
+{hashes}
+
+:date: {year}-{month}-{day} {hour}:{minute:02d}
+:tags:
+:category:
+:slug: {slug}
+:summary:
+:status: draft
+
+
+"""
+
+### function to create template file in content folder
+def make_entry(title):
+    today = datetime.today()
+    slug = title.lower().strip().replace(' ', '-')
+    f_create = "content/{}_{:0>2}_{:0>2}_{}.rst".format(
+        today.year, today.month, today.day, slug)
+    t = TEMPLATE.strip().format(title=title,
+                                hashes='#' * len(title),
+                                year=today.year,
+                                month=today.month,
+                                day=today.day,
+                                hour=today.hour,
+                                minute=today.minute,
+                                slug=slug)
+    with open(f_create, 'w') as w:
+        w.write(t)
+    print("File created -> " + f_create)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        make_entry(sys.argv[1])
+    else:
+        print "No title given"
