cb07187b8908118c4da932cbab6ffa884765c325
silent-mobius <alex@vaiolabs.com>
2019-04-13 11:15:29 +0300

    adding csv example

--- yaml_work.py
+++ yaml_work.py
@@ -17,4 +17,4 @@ int_name= yaml_dict['interface']['name']
 
 yaml_dict['interface']['name']['ipv4']['address'][0]['ip']='10.0.10.101'
 
-backtoyaml = yaml.dump(yaml_dict,default_flow_style=False)
+back_to_yaml = yaml.dump(yaml_dict,default_flow_style=False)
