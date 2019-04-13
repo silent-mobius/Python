39408e67e57b30efde119ed527d12ba2797bdbf2
silent-mobius <alex@vaiolabs.com>
2019-04-13 10:33:06 +0300

    adding json working example

---
+++ json_work.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pyhton3
+
+#json example
+import os
+import sys
+import time
+import json
+
+
+with open('json_example.json') as f:
+	json_ex = f.read
+	
+	
+
+json_dict = json_loads(json_ex)
+
+
+int_name = json_dict['interface']['name']
+
+
+json_dict['interface']['name']['address']['ip']='10.0.10.100'
+
+backtojson = json.dumps(json_dict)
