e86c594a7e1817aa4e976320b8b433f3824ca00a
pushtakio <alex@vaiolabs.com>
2019-09-26 20:06:22 -0400

    adding headers

--- ff_spoofer.py
+++ ff_spoofer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+#######################################################################
 # Python Network Programming Cookbook -- Chapter – 4
 # This program is optimized for Python 3.6 or newer.
 # It may run on any other version with/without modifications.
-
+#######################################################################
 import urllib.request
 
 BROWSER = 'Mozilla/5.0 (Windows NT 5.1; rv:20.0) Gecko/20100101Firefox/60.0'
