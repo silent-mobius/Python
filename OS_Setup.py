aa8d47e4823d57d312a167cfd30b349e27a1fc31
pushtakio <alex@vaiolabs.com>
2019-09-26 20:12:37 -0400

    adding header

--- OS_Setup.py
+++ OS_Setup.py
@@ -13,7 +13,7 @@
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
 #    * Neither the name of the <LinuxSystems LTD> nor the
-#      names of its contributors may be used to endorse or promote products
+#      ing headernames of its contributors may be used to endorse or promote products
 #      derived from this software without specific prior written permission.
 #
 #THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
