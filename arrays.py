4f72f40c749aefa36f708c512c28510cc57c4e5b
pushtakio <alex@vaiolabs.com>
2019-09-26 20:02:05 -0400

    renaming ex2.3

---
+++ arrays.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+########################################################################
+# created by: Pushtakio
+# purpose: practice arrays/lists
+# date: unknow
+# version: 1.0.1
+########################################################################
+
+var = 3
+arr = []
+per_arr=[1,2,3]
+
+while True:
+	if var > 0:
+		tmp = input(f'Please enter {var}st string: ')
+		arr.append(tmp)
+		var -= 1
+	else:
+		break
+
+print(arr)		
+arr.reverse()
+print(arr)
+arr.pop(1)
+arr.insert(1,per_arr)
+print(arr)
+a,b,c=arr
+print(b[1])
