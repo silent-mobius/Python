8f85678dcbc89ba28fc2f094f745dabe9d1f24a4
pushtakio <alex@vaiolabs.com>
2019-10-14 12:35:15 +0300

    adding descriptions

--- apache_log_parser.py
+++ apache_log_parser.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #############################################################
-#
-#
+# created by: O'Reilly (python for unix and linux administrators)
+# purpose:
 #
 #
 #############################################################
