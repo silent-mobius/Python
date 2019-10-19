2c4356f2bcf6274369d3c198ef3455127f594bf3
pushtakio <alex@vaiolabs.com>
2019-10-19 03:09:21 +0300

    fixing the object use

--- python3_guestbook.py
+++ python3_guestbook.py
@@ -3,6 +3,7 @@
 # Python Network Programming Cookbook -- Chapter - 5
 # This program is optimized for Python 3.6 or newer.
 # It may run on any other version with/without modifications
+#http://file.allitebooks.com/20150514/Python%20Network%20Programming%20Cookbook.pdf
 ########################################################################
 # created by: Pushtakio
 # purpose: practice cgi scripting
@@ -14,21 +15,21 @@ import os
 import cgi
 import argparse
 import http.server
-import CGIHTTPServer
+from http.server import CGIHTTPRequestHandler
 import cgitb
 cgitb.enable() ## enable CGI error reporting
 
 
 def web_server(port):
 	server = http.server.HTTPServer
-	handler = CGIHTTPServer.CGIHTTPRequestHandler #RequestsHandler
+	handler = CGIHTTPRequestHandler #RequestsHandler
 	server_address = ("", port)
 	handler.cgi_directories = ["/cgi-bin", ]
 	httpd = server(server_address, handler)
 	print(f'Starting web server with CGI support on port: {port}')
 	httpd.serve_forever()
-	
-	
+
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='CGI Server Example')
 	parser.add_argument('--port', action="store", dest="port",type=int, required=True)
