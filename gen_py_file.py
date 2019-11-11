0ee00411ebb34ef5a49f34c10be586815defb373
silent-mobius <alex@vaiolabs.com>
2019-11-12 00:18:22 +0200

    getting somewhere with this...

--- gen_py_file.py
+++ gen_py_file.py
@@ -18,7 +18,7 @@ msg_exists = "The filename you choose, already exists in current folder:\
 Pleae choose different name or move other folder"
 
 
-string_data='''
+string_data = '''
 #!/usr/bin/env python3
 
 #########################################################
@@ -28,12 +28,14 @@ string_data='''
 # Version: 1.0.0
 #########################################################
 '''
-line="##########################################################"
+
+line = "##########################################################"
+
 ## Funcs /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-def deco(msg_string):
-    print(line)
-    print('#', msg_string)
-    print(line)
+
+
+def deco(var):
+    print(line, '\n', '#', var, '\n', line)
 
 
 #####
@@ -41,15 +43,19 @@ def deco(msg_string):
 #####
 
 if __name__ == "__main__":
-    if sys.argv[1:] == '':
-        file_name = input("Please provide file name")
-        current_dir = os.getcwd()
-        if os.path.exists(f'{current_dir/file_name}'):
-            deco(msg_exists)
-        else:
-            with open('{current_dir/file_name}', 'x') as f:
-                f.write(string_data)
-            os.chmod(f'{current_dir/file_name}', 0o775)
+    file_name = sys.argv[1]
+    print(file_name)
+    file_name = input("Please provide file name")
+    #if file_name == '':
+    current_dir = os.getcwd()
+    #if os.path.exists(current_dir):
+    #        if os.path.exists(file_name):
+    #            deco(msg_exists)
+    #            sys.exit(1)
+    #else:
+    with open(file_name, 'x') as f:
+        f.write(string_data)
+    os.chmod(f'{file_name}', 0o775)
 
 
         
