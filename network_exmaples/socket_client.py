78938c864f75097e024c09809e55a86d344e2049
Pushtakio <alex@vaiolabs.com>
2020-01-12 16:19:34 +0200

    typo fix on socket_client

--- network_exmaples/socket_client.py
+++ network_exmaples/socket_client.py
@@ -11,11 +11,11 @@ import socket
 
 host = 'localhost'
 port = 8080
-
+data = ' '
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
 
-while i != 'EOF':
+while data != 'EOF':
     data = input('my message: ')
     s.send(data)
     d = s.recv(2048)
